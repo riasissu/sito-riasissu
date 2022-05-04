3cb1ed830a5a404ad0284e8d321c50b6caf67d54
vmcvastry <009valerio@gmail.com>
2022-05-04 18:48:48 +0200

    login working and signup setup+email confirmation (no check for verified email on login for debugging)

---
+++ public/auth/auth.js
@@ -0,0 +1,131 @@
+function signUp() {
+  //   var name = document.getElementById("userName").value;
+  //   var surname = document.getElementById("userSurname").value;
+  var name = "ciao";
+  var surname = "ciao";
+  var email = document.getElementById("userEmail").value;
+  var password = document.getElementById("userPassword").value;
+  //todo gather other user info
+
+  var checkUserEmailValid = email.match(emailRegex);
+  var checkUserPasswordValid = password.match(passwordRegex);
+
+  if (name == "") {
+    return checkName();
+  } else if (surname === "") {
+    return checkSurname();
+  } else if (checkUserEmailValid == null) {
+    return checkEmail();
+  } else if (checkUserPasswordValid == null) {
+    return checkPassword();
+  } else {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((success) => {
+        var user = firebase.auth().currentUser;
+        user.sendEmailVerification();
+        //TODO create user on DATABASE HERE
+        new swal(
+          "Your Account Created",
+          "Your account was created successfully check your email to confirm it"
+        ).then((value) => {
+          setTimeout(function () {
+            window.location.replace("../index.html");
+          }, 1000);
+        });
+      })
+      .catch((error) => {
+        new swal({
+          title: "Error",
+          text: error.message,
+        });
+      });
+  }
+}
+function logIn() {
+  var email = document.getElementById("userEmail").value;
+  var password = document.getElementById("userPassword").value;
+
+  var checkUserEmailValid = email.match(emailRegex);
+  var checkUserPasswordValid = password.match(passwordRegex);
+
+  if (checkUserEmailValid == null) {
+    return checkEmail();
+  } else if (checkUserPasswordValid == null) {
+    return checkPassword();
+  } else {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((success) => {
+        new swal({
+          title: "Succesfully signed in",
+        }).then((value) => {
+          setTimeout(function () {
+            window.location.replace("./pages/profile.html");
+          }, 1000);
+        });
+      })
+      .catch((error) => {
+        new swal({
+          title: "Error",
+          text: error.message,
+        });
+      });
+  }
+}
+
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(function () {
+      new swal({
+        type: "successfull",
+        title: "Signed Out",
+      }).then((value) => {
+        setTimeout(function () {
+          window.location.replace("../index.html");
+        }, 1000);
+      });
+    })
+    .catch(function (error) {
+      new swal({
+        title: "Error",
+        text: error.message,
+      });
+    });
+}
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (user) {
+//     //   User is signed in.
+//     let user = firebase.auth().currentUser;
+//     let uid;
+//     if (user != null) {
+//       uid = user.uid;
+//     }
+//     let firebaseRefKey = firebase.database().ref().child(uid);
+//     firebaseRefKey.on("value", (dataSnapShot) => {
+//       document.getElementById("userPfFullName").innerHTML =
+//         dataSnapShot.val().userFullName;
+//       document.getElementById("userPfSurname").innerHTML =
+//         dataSnapShot.val().userSurname;
+//       // userEmail = dataSnapShot.val().userEmail;
+//       // userPassword = dataSnapShot.val().userPassword;
+//       document
+//         .getElementById("userPfFb")
+//         .setAttribute("href", dataSnapShot.val().userFb);
+//       document
+//         .getElementById("userPfTw")
+//         .setAttribute("href", dataSnapShot.val().userTw);
+//       document
+//         .getElementById("userPfGp")
+//         .setAttribute("href", dataSnapShot.val().userGp);
+//       document.getElementById("userPfBio").innerHTML =
+//         dataSnapShot.val().userBio;
+//     });
+//   } else {
+//     //   No user is signed in.
+//   }
+// });
