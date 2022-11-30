fd0ef233de548afecfe429ef7d28cab1e68eca3e
Francesco <fradeca01@gmail.com>
2022-11-30 07:50:17 +0100

    refactor initializeapp

---
+++ frontend/src/firebase/initialize.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDDbBbCydrNYqW8o3lG0vnFvdtlhGzywgo",
+
+  authDomain: "sito-riasissu.firebaseapp.com",
+
+  projectId: "sito-riasissu",
+
+  storageBucket: "sito-riasissu.appspot.com",
+
+  messagingSenderId: "81670035087",
+
+  appId: "1:81670035087:web:433f388c9fe06a522842fa",
+
+  measurementId: "G-WQJ6WP2TKN"
+
+};
+
+initializeApp(firebaseConfig)
