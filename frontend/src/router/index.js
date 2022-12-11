721a0db48919ec95b6096b651050339850424cb3
Francesco <fradeca01@gmail.com>
2022-12-11 20:19:37 +0100

    vue3

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,49 +10,51 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path:'/login',
-    name:'login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')  
-  },
-  {
-    path:'/signup',
-    name:'signup',
-    component: () => import(/* webpackChunkName: "signup" */ '../views/SignUpView.vue')  
-  },
-  {
-    path:'/profile',
-    name:'profile',
-    component:()=>import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue')
-  },
-  {
-    path:'/scuole',
-    name:'scuole',
-    component:()=>import(/* webpackChunkName: "profile" */ '../views/MappaView.vue')
-  },
-  {
-    path:'/chiasmo',
-    name:'chiasmo',
-    component:()=>import(/* webpackChunkName: "profile" */ '../views/ChiasmoView.vue')
-  },
-  {
-    path:'/reteidee',
-    name:'reteidee',
-    component:()=>import(/* webpackChunkName: "profile" */ '../views/ReteIdeeView.vue')
-  },
-  {
-    path:'/xcool',
-    name:'xcool',
-    component:()=>import(/* webpackChunkName: "profile" */ '../views/XCoolView.vue')
-  },
+//  {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   },
+ 
+//   {
+//     path:'/login',
+//      name:'login',
+//   //component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')  
+//   },
+//   {
+//     path:'/signup',
+//     name:'signup',
+//     //component: () => import(/* webpackChunkName: "signup" */ '../views/SignUpView.vue')  
+//   },
+//   {
+//     path:'/profile',
+//     name:'profile',
+//     //component:()=>import(/* webpackChunkName: "profile" */ '../views/ProfileView.vue')
+//   },
+//   {
+//     path:'/scuole',
+//     name:'scuole',
+//     //component:()=>import(/* webpackChunkName: "profile" */ '../views/MappaView.vue')
+//   },
+//   {
+//     path:'/chiasmo',
+//     name:'chiasmo',
+//     //component:()=>import(/* webpackChunkName: "profile" */ '../views/ChiasmoView.vue')
+//   },
+//   {
+//     path:'/reteidee',
+//     name:'reteidee',
+//    // component:()=>import(/* webpackChunkName: "profile" */ '../views/ReteIdeeView.vue')
+//   },
+//   {
+//     path:'/xcool',
+//     name:'xcool',
+//     component:()=>import(/* webpackChunkName: "profile" */ '../views/XCoolView.vue')
+//   },
+  
   //{
   //  path:'/scuole',
   //  name:'scole',
